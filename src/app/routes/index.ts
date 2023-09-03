2b7398c7948016679ef09b6654e4c45343753d92
afzal75 <afzalbhuiyan16@gmail.com>
2023-09-04 02:09:49 +0600

    all user fetched data loaded successfully

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { AuthRoutes } from '../modules/auth/auth.routes';
+import { UserRoutes } from '../modules/user/user.routes';
 
 const router = express.Router();
 
@@ -8,6 +9,10 @@ const moduleRoutes = [
     path: '/auth',
     route: AuthRoutes,
   },
+  {
+    path: '/users',
+    route: UserRoutes
+  }
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
